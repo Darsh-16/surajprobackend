d176c6dcaf3b0636dca99a4a31186262093d33b3
dpy16 <ganeshmahadev16@gmail.com>
2025-01-25 03:47:51 +0530

    ccnew

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,8 @@
 // DataBase Connection
 const mongoose = require("mongoose")
 mongoose.set("strictQuery", true)
-mongoose.connect("mongodb://127.0.0.1:27017/bulkmedicine", {
+// mongoose.connect("mongodb://127.0.0.1:27017/bulkmedicine", {
+    mongoose.connect("mongodb+srv://rikcapital:rikcapital@rikcapital.eb7pg.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connection successfully"))
