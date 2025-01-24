38df7aa41fc7edd5d5b1b598ecb59ee8d0feaad2
dpy16 <ganeshmahadev16@gmail.com>
2025-01-25 04:55:32 +0530

    ccnewbulk

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose")
 mongoose.set("strictQuery", true)
 // mongoose.connect("mongodb://127.0.0.1:27017/bulkmedicine", {
-    mongoose.connect("mongodb+srv://idarshprajapatikrb:2F0vXLDQoDAT8BmI@bulkbsense.m4abh.mongodb.net/?retryWrites=true&w=majority&appName=Bulkbsense", {
+    mongoose.connect("mongodb+srv://idarshprajapatikrb:2F0vXLDQoDAT8BmI@bulkbsense.m4abh.mongodb.net/bulkdata", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connection successfully"))
